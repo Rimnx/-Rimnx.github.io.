b4e917a7c111e23447349d4a78ddb710a7b8461d
Rimnx <urieasia@gmail.com>
2024-03-15 22:14:29 -0600

    finished notes input and added more js code

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,20 @@
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-
-for (let x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+function listTopics() {
+   for (let x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+   }
 }
-/*
-if (topics === 'HTML') {
+
+function selectTopic() {
+if (topic === 'HTML') {
     console.log("Let's study HTML!");
-} else if (topics === 'CSS') {
+} else if (topic === 'CSS') {
     console.log("Let's study CSS!");
- } else if (topics === 'Git') {
+ } else if (topic === 'Git') {
     console.log("Let's study Git!");
- } else if (topics === 'JavaScript') {
+ } else if (topic === 'JavaScript') {
     console.log("Let's study JavaScript!");
  } else {
     console.log('Please try again!');
  }
-*/
+}
